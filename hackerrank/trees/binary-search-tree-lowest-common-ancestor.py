bf04e6ff378fb7799ba9acecb6f9009ffc456ec4
Johannes Heinzl <jh.heinzl@gmail.com>
2024-01-07 15:28:04 +0100

    Updates.

---
+++ hackerrank/trees/binary-search-tree-lowest-common-ancestor.py
@@ -0,0 +1,96 @@
+class Node:
+    def __init__(self, info): 
+        self.info = info  
+        self.left = None  
+        self.right = None 
+        self.level = None 
+
+    def __str__(self):
+        return str(self.info) 
+
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
+
+    def create(self, val):  
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+         
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+'''
+class Node:
+      def __init__(self,info): 
+          self.info = info  
+          self.left = None  
+          self.right = None 
+           
+
+       // this is a node of the tree , which contains info as data, left , right
+'''
+
+def lca(root, v1, v2):
+    #Enter your code here
+    
+    if v1 > v2:
+        tmp = v1
+        v1 = v2
+        v2 = tmp
+    
+    #if root is None:
+    #    return None
+    
+    if root.left is not None and root.left.info == v1 and root.info < v2:
+        return root
+        
+    if root.right is not None and root.right.info == v2 and root.info > v1:
+        return root
+    
+    if v1 < root.info and v2 > root.info:
+        return root
+    
+    if v1 < root.info and v2 < root.info:
+        return lca(root.left, v1, v2)
+        
+    if v1 > root.info and v2 > root.info:
+        return lca(root.right, v1, v2)
+    
+    if root.info == v1 and ((root.left is not None and root.left.info == v2) or (root.right is not None and root.right.info == v2)):
+        return root
+    
+    if root.info == v2 and ((root.left is not None and root.left.info == v1) or (root.right is not None and root.right.info == v1)):
+        return root
+    
+    if root.info == v1 or root.info == v2:
+        return root
+
+tree = BinarySearchTree()
+t = int(input())
+
+arr = list(map(int, input().split()))
+
+for i in range(t):
+    tree.create(arr[i])
+
+v = list(map(int, input().split()))
+
+ans = lca(tree.root, v[0], v[1])
+print (ans.info)
+
